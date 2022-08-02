3014a51c1a9fd1be19fd4ac9f1d221b477b4cb27
sf200212345 <stevefan@umich.edu>
2022-08-02 17:11:01 -0400

    Resolved #8. Adding transaction should be clearer now.

--- src/components/AddTransactions.js
+++ src/components/AddTransactions.js
@@ -10,12 +10,21 @@ const AddTransactions = () => {
 
     const submit = e => {
       e.preventDefault();
-
-      addTransaction({
-        id: nextID,
-        text,
-        number: +number
-      })
+      console.log(e.nativeEvent.submitter.value);
+      if (e.nativeEvent.submitter.value === "income") {
+        addTransaction({
+          id: nextID,
+          text,
+          number: +number
+        });
+      }
+      else {
+        addTransaction({
+          id: nextID,
+          text,
+          number: +number * -1
+        });
+      }
       changeSort("R");
     }
 
@@ -24,10 +33,13 @@ const AddTransactions = () => {
         <h3>Add Transactions</h3>
         <form onSubmit={submit}>
             <label htmlFor="text">Classify Transaction Type:</label>
-            <input type="text" id="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter Text..." />
-            <label htmlFor="number">Enter Amount (negative = expense, positive = income):</label>
-            <input type="number" id="number" value={number} onChange={(e) => setNumber(e.target.value)} placeholder="Enter Amount..." />
-            <button type="submit">Add</button>
+            <input type="text" required id="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter Text..." />
+            <label htmlFor="number">Enter Amount:</label>
+            <input type="number" required id="number" value={number} onChange={(e) => setNumber(e.target.value)} placeholder="Enter Amount..." />
+            <div className="btns">
+              <button className='in' type="submit" value="income">Add as Income</button>
+              <button className="out" type="submit" value="expense">Add as Expense</button>
+            </div>
         </form>
     </div>
   )
